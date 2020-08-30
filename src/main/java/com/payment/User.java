8d40aee05766129603d78301534fc699df293c59
Marina Shvachko <email.for.marina@gmail.com>
2020-08-30 13:45:56 +0300

    Add classes: User, Account, App

---
+++ src/main/java/com/payment/User.java
@@ -0,0 +1,37 @@
+package com.payment;
+
+import java.math.BigDecimal;
+
+public class User {
+    private String phoneNumber;
+    private String name;
+    private String familyname;
+    private Account account;
+
+    public User(String phoneNumber, String name, String familyname, int accountBalance, int clientNumber, int bankAccountNumber) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.familyname = familyname;
+        this.account = new Account();
+        account.setAccountBalance(BigDecimal.valueOf(accountBalance));
+        account.setCurrency("Ru");
+        account.setClientNumber(clientNumber);
+        account.setBankAccountNumber(bankAccountNumber);
+    }
+
+    public BigDecimal getAccountBalance() {
+        return account.getAccountBalance();
+    }
+
+    public String getCurrency() {
+        return account.getCurrency();
+    }
+    public int getClientNumber() {
+        return account.getClientNumber();
+    }
+
+    public int getBankAccountNumber() {
+        return account.getBankAccountNumber();
+    }
+
+}
