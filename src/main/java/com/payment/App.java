4d9cbdbf66062e5a35902f8cdb7fc6b3f15a9e80
Marina Shvachko <email.for.marina@gmail.com>
2020-09-09 00:00:50 +0300

    В класс App добавила проверку на длину номера телефона

--- src/main/java/com/payment/App.java
+++ src/main/java/com/payment/App.java
@@ -10,26 +10,35 @@ public class App implements Pay, ReceiveDataForPayment {
         private BigDecimal paymentAmount;
 
         //узнать номер телефона и сумму, которую отправим по номеру телефона
-        public ArrayList<String> ReceivePhoneNumber() {
+        public ArrayList<String> ReceivePhoneNumberAndMoney() {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                String phoneNumber="";
-                String amountToPay="";
+                String phoneNumber = "";
+                String amountToPay = "";
                 ArrayList<String> phoneAndAmount = new ArrayList<String>();
                 System.out.println("Введите номер телефона в формате 89121115533");
 
-                try {
-                        phoneNumber = reader.readLine();
-                        System.out.println("Введите сумму для перевода");
-                        amountToPay = reader.readLine();
-                }  catch (IOException e) {
-                        System.out.println(e);
-                }
-                phoneAndAmount.add(phoneNumber);
-                phoneAndAmount.add(amountToPay);
-                return phoneAndAmount;
-        }
+                        try {
+                                phoneNumber = reader.readLine();
+                                while (phoneNumber.length() < 11) {
+                                        System.out.println("короткий, введите полный номер");
+                                        phoneNumber = reader.readLine();
+                                }
+                                        System.out.println("Введите сумму для перевода");
+                                        amountToPay = reader.readLine();
 
-        //оплатить
+                        } catch (IOException e) {
+                                System.out.println(e);
+                        }
+
+                        phoneAndAmount.add(phoneNumber);
+                        phoneAndAmount.add(amountToPay);
+
+                        return phoneAndAmount;
+                }
+
+
+
+        //оплатить: кто, сколько, кому
         public void pay() { //получает данные и передает их на сервер
         }
 }
